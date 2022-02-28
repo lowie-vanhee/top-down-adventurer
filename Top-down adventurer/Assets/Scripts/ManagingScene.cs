94fae2934d9dd20b7a3bfa5bef4417a2eedcc95b
Lowie Vanhee <lowie.vanhee@telenet.be>
2022-02-28 22:29:16 +0100

    Added Options menu

--- Top-down adventurer/Assets/Scripts/ManagingScene.cs
+++ Top-down adventurer/Assets/Scripts/ManagingScene.cs
@@ -25,4 +25,9 @@ public class ManagingScene : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void GoToOptionsMenu()
+    {
+        SceneManager.LoadScene("OptionsMenu");
+    }
 }
